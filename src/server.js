d9a9b31f1070677b25aae49385f95864dea35f1e
JadeYoon <yess9904@naver.com>
2023-01-29 20:32:25 +0900

    middleware part.1

--- src/server.js
+++ src/server.js
@@ -4,15 +4,20 @@ const PORT = 4000;
 
 const app = express();
 
-const handleHome = (req, res) => {
-    return res.send("I'm dying");
+const gossipMddleware = (req, res , next) => {
+    console.log("I'm in the middle");
+    next();
+}
+
+const handleHome = (req, res, next) => {
+    return res.end();
 };
 
 const handelLogin = (req, res) => {
     return res.send("<h1>Login here</h1>");
 };
 
-app.get("/",  handleHome);
+app.get("/", gossipMddleware, handleHome);
 app.get("/login", handelLogin);
 
 const handleListening = () => console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
