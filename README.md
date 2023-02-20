26e0c25db6ad435a06c1d3703a01172adc13bca9
JadeYoon <yess9904@naver.com>
2023-02-20 23:17:04 +0900

    URL Parameters

---
+++ README.md
@@ -0,0 +1,18 @@
+# Wetube Reloaded
+
+/ -> home
+/join -> Join
+/login -> Login
+/search -> Search
+
+/users/:id -> See user
+/users/logout -> Log out
+/users/edit -> Edit my profile
+/users/delete -> Delete my profile
+
+/videos/:id -> Watch video
+/videos/:id/edit -> Edit video
+/videos/:id/delete -> Delete video
+/videos/upload -> Upload video
+
+https://expressjs.com/en/guide/routing.html
--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,11 +1,13 @@
 import express from "express";
-import {trending} from "../controllers/videoControllers";
-import {join} from "../controllers/userControllers";
+import {trending, search} from "../controllers/videoControllers";
+import {join, login} from "../controllers/userControllers";
 
 const globalRouter = express.Router();
 
 // reason why not using folder: globalRouter is just for pretty urls
 globalRouter.get("/", trending);
 globalRouter.get("/join", join);
+globalRouter.get("/login", login);
+globalRouter.get("/search", search);
 
 export default globalRouter;
