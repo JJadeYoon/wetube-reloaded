d65638197d88392a1f2fcfdf1cd212ff73b5b0c5
JadeYoon <yess9904@naver.com>
2023-03-05 21:29:54 +0900

    Edit Video

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,13 +1,11 @@
 import express from "express";
-import {trending, search} from "../controllers/videoControllers";
+import {trending} from "../controllers/videoControllers";
 import {join, login} from "../controllers/userControllers";
 
 const globalRouter = express.Router();
 
-// reason why not using folder: globalRouter is just for pretty urls
 globalRouter.get("/", trending);
 globalRouter.get("/join", join);
 globalRouter.get("/login", login);
-globalRouter.get("/search", search);
 
 export default globalRouter;
